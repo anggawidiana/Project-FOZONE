639d54b63278033e809a78dc125235e67a12aa49
gnsz <gungangga28@gmail.com>
2024-07-04 22:30:41 +0800

    scroll animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // script.js
+// Navbar Start
 const navbarLinks = document.querySelectorAll(".navbar a");
 
 navbarLinks.forEach((link) => {
@@ -9,9 +10,27 @@ navbarLinks.forEach((link) => {
     this.classList.add("active");
   });
 });
+//Navbar End
 
+//Klik Kanan
 document.addEventListener("contextmenu", function (e) {
   if (e.target.tagName === "IMG" && e.target.closest(".about")) {
     e.preventDefault();
   }
 });
+//Klik Kanan
+
+//scroll animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
