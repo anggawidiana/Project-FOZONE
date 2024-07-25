357c547fb5dd58d43993b13bce59f5636474e010
gnsz <gungangga28@gmail.com>
2024-07-25 22:06:01 +0800

    delay preloader

--- galery/script.js
+++ galery/script.js
@@ -1,4 +1,15 @@
-const loader = document.getElementById("preloader");
-window.addEventListener("load", function () {
-  loader.style.display = "none";
+// const loader = document.getElementById("preloader");
+// window.addEventListener("load", function () {
+//   loader.style.display = "none";
+//   setTimeout(function () {
+//     preload.classList.add("preload-finish");
+//   }, 1000);
+// });
+
+window.addEventListener("load", () => {
+  const preload = document.getElementById("preloader");
+  preload.style.display = "none";
+  setTimeout(function () {
+    preload.classList.add("preload-finish");
+  }, 5000);
 });
