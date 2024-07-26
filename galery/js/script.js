86bd8be4359693203b86325fade60b4c60fa237d
gnsz <gungangga28@gmail.com>
2024-07-26 23:13:46 +0800

    loader bug

--- galery/js/script.js
+++ galery/js/script.js
@@ -1,16 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const loader = document.getElementById("preloader");
-
-  // Simpan dalam variabel dan tambahkan event listener
-  window.addEventListener("load", function () {
-    // Delay untuk menunggu gambar dan elemen lainnya dimuat
-    setTimeout(function () {
-      loader.style.opacity = 0; // Tambahkan efek fade-out
-      setTimeout(function () {
-        loader.style.display = "none"; // Sembunyikan preloader setelah efek fade-out
-      }, 500); // Waktu delay untuk efek fade-out
-    }, 2000); // Waktu tampil preloader
-  });
+const loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 3000); // Timeout duration in milliseconds (3000 ms = 3 seconds)
 });
 
 document.addEventListener("DOMContentLoaded", function () {
